67c0056243a116f57cf5699b8d9a56ad1a47b4d1
Karunagaran Ulaganathan <karunagu@amazon.com>
2019-04-19 15:50:40 -0700

    JS code for all images added

--- memorygame.js
+++ memorygame.js
@@ -1,1 +1,53 @@
 'use strict';
+
+//Variables
+var numberCells = 8;
+var locationList = [];
+var i,j = 0;
+
+//Array to store all the images
+MemoryItem.allImages = [];
+
+
+
+// create objects
+function MemoryItem(name, filepath) {
+  this.name = name;
+  this.filepath = filepath;
+  this.loc1 = 0;
+  this.loc2 = 0;
+  this.statusLoc1 = 'c';
+  this.statusLoc2 = 'c';
+  this.completed = 0;
+  MemoryItem.allImages.push(this);
+}
+
+new MemoryItem('dubai', 'img/dubai.jpg');
+new MemoryItem('newyork', 'img/newyork.jpg');
+new MemoryItem('rome', 'img/rome.jpg');
+new MemoryItem('amsterdam', 'img/amsterdam.jpg');
+
+// Generate an array with list of random numbers
+// function randomCell() {
+for (i = 1; i <= numberCells; i++) {
+  var randomIdx = (Math.ceil(Math.random() * numberCells));
+  console.log('i', randomIdx);
+  j = 1;
+  while (j < locationList.length) {
+    if (locationList[j] === randomIdx) {
+      randomIdx = (Math.ceil(Math.random() * numberCells));
+      j = 1;
+    }
+    j++;
+
+  }
+  locationList[i] = randomIdx;
+}
+i = 0;
+j = 1;
+while (i < MemoryItem.allImages.length) {
+  MemoryItem.allImages[i].loc1 = locationList[j];
+  MemoryItem.allImages[i].loc2 = locationList[j + 1];
+  i++;
+  j = j + 2;
+}
