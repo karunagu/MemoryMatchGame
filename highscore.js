5b26f316aee287c21929e291c1bd5d0dbea234d4
Vinay Kumar Adireddy <avk@Vinays-MacBook-Pro.local>
2019-05-09 23:44:41 -0700

    resultpage

---
+++ highscore.js
@@ -0,0 +1,34 @@
+
+//Search
+'use strict';
+
+Result.allResults = [];
+var table = document.getElementById('displayResult'); //Get
+
+//Result object
+function Result(name, clickCount, datePlayed) {
+  this.name = name;
+  this.clickCount = clickCount;
+  this.datePlayed = datePlayed;
+  Result.allResults.push(this);
+}
+function getResults() {
+  //retrieve results already stored
+  var storageResults = localStorage.getItem('memorygame.results');
+  if (storageResults) {
+    Result.allResults = JSON.parse(storageResults);
+  }
+}
+//display the results in table rows
+function render() {
+  for (var i = 0; i < Result.allResults.length; i++) {
+    var newRow = document.createElement('tr');
+    var eachElement = Result.allResults[i];
+    newRow.innerHTML = `<td> ${eachElement.name} </td> <td> ${eachElement.clickCount} </td> <td> ${eachElement.datePlayed} </td>`;
+    table.appendChild(newRow);
+  }
+}
+getResults();
+render();
+console.log('end here');
+
